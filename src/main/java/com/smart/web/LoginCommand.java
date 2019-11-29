b1b20f69d29ed353380d00c5ea19ee8c8f31049e
wangxiao <1595990651@qq.com>
2019-11-29 21:58:03 +0800

    spring login demo

---
+++ src/main/java/com/smart/web/LoginCommand.java
@@ -0,0 +1,27 @@
+package com.smart.web;
+
+/**
+ * @author ruin
+ * @date 2019/11/29-21:11
+ */
+public class LoginCommand {
+
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/com/smart/service/UserServiceTest.java
@@ -0,0 +1,55 @@
+package com.smart.service;
+
+import com.smart.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+
+
+import java.util.Date;
+
+import static org.testng.Assert.*;
+
+
+/**
+ * @author ruin
+ * @date 2019/11/29-19:58
+ */
+//启动Spring容器
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:smart-context.xml")
+public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests{
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void testHasMatchUser() {
+        boolean b1 = userService.hasMatchUser("admin", "123456");
+        boolean b2 = userService.hasMatchUser("admin", "1111");
+        assertTrue(b1);
+        assertTrue(!b2);
+    }
+
+    @Test
+    public void testFindUserByUserName()throws Exception{
+        for(int i =0; i< 100;i++) {
+            User user = userService.findUserByUserName("admin");
+            assertEquals(user.getUserName(), "admin");
+        }
+
+    }
+
+    @Test
+    public void testAddLoginLog() {
+        User user = userService.findUserByUserName("admin");
+        user.setUserId(1);
+        user.setUserName("admin");
+        user.setLastIp("192.168.12.7");
+        user.setLastVisit(new Date());
+        userService.loginSuccess(user);
+    }
+}
